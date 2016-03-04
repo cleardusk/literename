eeb2a39a34d4dd453828292100f1ad6c8868efee
cleardusk <guojianzhu1994@foxmail.com>
2016-03-04 20:24:13 +0800

    Demo

--- main.py
+++ main.py
@@ -15,12 +15,17 @@ def get_pdf_info2(filepath=None):
 	pdf_info = pdf_rd.getDocumentInfo()
 	return pdf_info
 
+def extract_text(filepath=None):
+	pdf_rd = PyPDF2.PdfFileReader(open(filepath, 'rb'))
+	page_one = pdf_rd.getPage(0)
+	return page_one.extractText()
+
 def get_suffix(filepath=None):
 	return filepath[filepath.rfind('.') + 1 :].lower()
 
 def main():
 	# filename = './test/imagenet.pdf'
-	data_dir = './test'
+	data_dir = './test/ieee'
 
 	num_has_title, num_total = 0, 0
 	for flnm in os.listdir(data_dir):
@@ -29,14 +34,17 @@ def main():
 			continue
 
 		ret = get_pdf_info(flpath)
-		print ret
-		print
+		# print ret
+		# print
 
-		if ret.has_key(u'/Title') and ret[u'/Title'] != u'':
-			num_has_title += 1
 		num_total += 1
+		if ret.has_key(u'/Title') and ret[u'/Title'] != u'':
+			print '##%d ' % num_total + ret['/Title']
+			num_has_title += 1
+		else:
+			print '$$%d ' % num_total + extract_text(flpath)[:50]
 
-	print '%d / %d' % (num_has_title, num_total)
+	print '\n%d / %d' % (num_has_title, num_total)
 
 if __name__ == '__main__':
 	main()
